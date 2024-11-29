44dcdf62992432bc697e0d1f437cabc2200340e0
Eduardo Diogo <educdiogo@hotmail.com>
2024-11-28 21:53:18 -0400

    feat: New Trasaction Modal Styles

--- src/pages/Transactions/components/SearchForm/styles.ts
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -25,16 +25,17 @@ export const SearchFormContainer = styled.form`
     border: 0;
     padding: 1rem;
     background: transparent;
-    border: ${props => props.theme['green-300']};
-    color: ${props => props.theme['green-300']};
+    border: 1px solid ${props => props.theme["green-300"]};
+    color: ${props => props.theme["green-300"]};
     font-weight: bold;
-    border-radius: 6px; 
+    border-radius: 6px;
+    cursor: pointer;
 
     &:hover {
-      background: ${props => props.theme['green-500']};
-      border: ${props => props.theme['green-500']};
-      color: ${props => props.theme.white};
-      transition: background 0.2s, color 0.2s, border-color 0.2s;
+      background-color: ${props => props.theme["green-500"]};
+      border-color: ${props => props.theme["green-500"]};
+      color: ${props => props.theme["white"]};
+      transition: background-color 0.2s, color 0.2s, border-color 0.2s;
     }
   }
 `
